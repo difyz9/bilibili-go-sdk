f5ff0d0a4ae89fe07227de99bf5f084117419abe
difyz <15564773+difyz@user.noreply.gitee.com>
2026-04-03 10:13:04 +0800

    update

---
+++ examples/zone/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/difyz9/bilibili-go-sdk/bilibili"
+)
+
+func main() {
+
+	zones := bilibili.GetVideoZones()
+	fmt.Printf("main zones: %d\n", len(zones))
+	for _, zone := range zones {
+		fmt.Printf("- %s (tid=%d)\n", zone.Name, zone.ID)
+	}
+
+	allZones := bilibili.GetAllVideoZones()
+	fmt.Printf("all zones: %d\n", len(allZones))
+
+	zone, ok := bilibili.GetVideoZoneByTID(265)
+	if !ok {
+		log.Fatal("expected tid 265 to exist")
+	}
+	fmt.Printf("zone for tid 265: %s (parent tid=%d)\n", zone.Name, zone.ParentID)
+
+	matchesByTID := bilibili.FindVideoZonesByTID(176)
+	fmt.Printf("matches for tid 176: %d\n", len(matchesByTID))
+	for _, match := range matchesByTID {
+		fmt.Printf("- %s (tid=%d)\n", match.Name, match.ID)
+	}
+
+	matchesBySlug := bilibili.FindVideoZonesBySlug("information")
+	fmt.Printf("matches for slug 'information': %d\n", len(matchesBySlug))
+	for _, match := range matchesBySlug {
+		fmt.Printf("- %s (tid=%d)\n", match.Name, match.ID)
+	}
+
+}
